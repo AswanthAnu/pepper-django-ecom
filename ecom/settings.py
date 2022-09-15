5f5adf3a7a4a690ae22d6ec06875d0bbe02fbc18
AswanthAnu <aswanthanu777@gmail.com>
2022-09-15 19:18:21 +0530

    'allnew'

--- ecom/settings.py
+++ ecom/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pepperecommerce-env.eba-4ym5mbv2.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
